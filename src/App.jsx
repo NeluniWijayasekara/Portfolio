3360f0c1dc3e394d0a0ab2b0089943186a122e78
Nelu <neluni09@gmail.com>
2025-01-27 20:12:24 +0530

    create component technologies.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
-import Hero from './Components/Hero'
-import About from './Components/About'
+import React from 'react';
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+import About from './Components/About';
+import Technologies from './Components/Technologies';
 
 const App = () => {
   return (
-    <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
-      <div className='fixed top-0 -z-10 h-full w-full'></div>
-      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-      <div className='container mx-auto px-8'>
-      <Navbar/>
-      <Hero/>
-      <About/>
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+      {/* Background layers */}
+      <div className="fixed top-0 -z-10 h-full w-full bg-neutral-950"></div>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      
+      {/* Main container */}
+      <div className="container mx-auto px-8">
+        <Navbar />
+        <Hero />
+        <About />
+        <Technologies/>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
